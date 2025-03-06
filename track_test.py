b0bf7fd9c501f3f2e194413a11f1fdef0e9d9b1e
MoffKalast <vid.rijavec@gmail.com>
2025-03-06 17:03:13 +0100

    tracking reset, min score

--- track_test.py
+++ track_test.py
@@ -45,11 +45,14 @@ def get_people(results):
 		labels = result.boxes.cls.cpu().numpy()  # Class IDs
 		
 		for box, score, label in zip(boxes, scores, labels):
-			if model.names[int(label)] == "person":
+			if model.names[int(label)] == "person" and score > 0.6:
 				x1, y1, x2, y2 = map(int, box)
 				people_coords.append(((x1+x2)/2,(y1+y2)/2))
 
 	return people_coords
+
+def move_forward():
+	control.absolute_move(-75, 56, 1.0, speed)
 
 IP = '10.32.38.127'
 speed = 20
@@ -57,6 +60,9 @@ speed = 20
 sent_stamp = time.time()
 
 control = PTZControl(VapixAPI(IP, '', ''))
+control.set_autofocus(True)
+move_forward()
+
 stream_url = f'http://{IP}/mjpg/video.mjpg'
 
 model = YOLO("yolo11s.pt")
@@ -73,8 +79,17 @@ while (stream.isOpened()):
 		cv2.imshow('Video Stream Monitor', img)
 		cv2.waitKey(1)
 
+		deltatime = time.time() - sent_stamp 
+
 		people = get_people(results)
-		if len(people) > 0 and time.time() - sent_stamp > 0.3:
+		if len(people) > 0 and deltatime > 0.3:
 			sent_stamp = time.time()
 			pos = people[0] #just move to the first person for now
-			control.center_move(pos[0], pos[1], 20)
+			control.center_move(pos[0], pos[1], speed)
+
+		#haven't seen anyone for a bit?
+		if deltatime > 40 and deltatime < 41:
+			sent_stamp -= 2
+			move_forward()
+			print("Resetting!")
+
